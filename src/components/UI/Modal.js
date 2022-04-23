f911a3a2219b529823d4f7cff283fdabea25b549
Hayeun Lee <whitehy@gmail.com>
2022-04-23 14:36:47 -0700

    Managing Cart & Modal State

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -5,7 +5,7 @@ import classes from "./Modal.module.css";
 
 
 const Backdrop = (props) => {
-  return <div className={classes.backdrop} />;
+  return <div className={classes.backdrop} onClick={props.onClose} />;
 };
 const ModalOverlay = (props) => {
   return (
@@ -19,7 +19,7 @@ const portalElement = document.getElementById("overlays");
 const Modal = (props) => {
   return (
     <Fragment>
-      {ReactDom.createPortal(<Backdrop />, portalElement)}
+      {ReactDom.createPortal(<Backdrop onClose={props.onClose} />, portalElement)}
       {ReactDom.createPortal(
         <ModalOverlay>{props.children}</ModalOverlay>,
         portalElement
